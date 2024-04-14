553eef5c85375429eef95e71946b0822a5dbbcb3
Theodor <theodornm@gmail.com>
2024-04-14 17:12:34 +0200

    Added coastline plotting

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import json
 
 def load_fligt(path: str) -> list[list[str]]:
     with open(path, "rt", encoding="utf-8") as csv_file:
@@ -9,11 +10,38 @@ def load_fligt(path: str) -> list[list[str]]:
 
 def handle_data(flight_data: list[list[str]]):
     callsign = flight_data[0][2]
-    for row in flight_data[1:]:
+    lats = []
+    longs = []
+    for row in flight_data:
         pos = row[3].split(",")
-        lat = pos[0]
-        long = pos[1]
 
+        lats.append(pos[0])
+        longs.append(pos[1])
+    
+def read_file(filnavn):
+    with open(filnavn, 'r', encoding='utf-8') as filobjekt:
+        return filobjekt.read()
 
+def load_coastlines():
+    string = read_file("data/ne_110m_coastline.json")
+    d = json.loads(string)
+    islands = d['features']
+    cords = []
+    for island in islands:
+        island_cords = island['geometry']['coordinates']
+        cords.append(island_cords)
+    return cords
+
+def plot_coastlines(lines):
+    for line in lines:
+        xs = []
+        ys = []
+        for cord in line:
+            xs.append(cord[0])
+            ys.append(cord[1])
+        plt.plot(xs,ys, color='grey')
 
 load_fligt("data/KL1341_34bda3c5.csv")
+plt.figure(figsize=(12,8))
+plot_coastlines(load_coastlines())
+plt.show()
